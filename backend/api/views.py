7aa086ad27cec3da433b4b3987da4c5cd20ea3bf
farnoushnazari <farnoushnazari98@gmail.com>
2025-02-13 08:19:50 +0330

    Create Product app and add basic setting

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,15 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
+from products.models import Product
 
-
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there"})
+    """
+    DRF API View
+    """
+    model_data = Product.objects.all().first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response({"message": data})
